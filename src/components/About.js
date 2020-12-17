5bc2a8b0d43742bd070db6343714de6dd73c177b
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2020-12-16 17:27:09 -0800

    updated about page styles
    also added text in

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,14 @@ const About = () => {
             <div class="content-box">
                 <h1>About</h1>
                 <hr />
-                <p>some placeholder about text here, this is where the description about the app goes</p>
+                <p>Keep track of your favourite movies with Watcher. Our application allows
+                    users to browse through their favourite movies, check their ratings, and
+                    add them to their lists. Don't like a movie anymore? Removing it from your
+                    favourites is as easy as pressing the heart button on the posters.
+                </p>
+                <p>
+                    Add TMDB disclaimer here later
+                </p>
             </div>
             
         </main>
