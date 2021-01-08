792169ba62a363f52f9175c59176a649fc1b20c0
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-01-07 17:21:49 -0800

    First attempts at setting up api

---
+++ src/data/PopularMovies.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import { TMDB_API_KEY, TMDB_BASE_URL } from '../globals/variables';
+
+const PopularMovies = () => {
+
+    const fetchPopularMovies = async () => {
+        const res = await fetch(`${TMDB_BASE_URL}popular?api_key={TMDB_API_KEY}&language=en-US&page=1$`)
+        let data = await res.json()
+        return data
+    }
+}
+
+export default fetchPopularMovies
--- src/globals/variables.js
+++ src/globals/variables.js
@@ -1,3 +1,5 @@
 //TMDB documentation https://www.themoviedb.org/documentation/api
-const TMDB_API_KEY = '8cbb2f7768a12caf72305ec05d30851a'
+const TMDB_API_KEY_ONLY = `8cbb2f7768a12caf72305ec05d30851a`
+export const TMDB_API_KEY = `?api_key=${TMDB_API_KEY_ONLY}`
+export const TMDB_BASE_URL = `https://api.themoviedb.org/3/movie/`
 
