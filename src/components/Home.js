aad6b394577e441d1b054506302ada50f4317215
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2021-01-07 16:02:57 -0800

    12 posters requirement

--- src/components/Home.js
+++ src/components/Home.js
@@ -119,6 +119,126 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="movie-info-card">
+                        <div class="poster">
+                            <img src={poster} />
+                        </div>
+                        <div class="description">
+                            <div class="hover-info">
+                                <button class="poster-fave-button">heart</button>
+                                <p>placeholder movie info here, to be seen on hover</p>
+                                <button class="more-info-btn">More info</button>
+                            </div>
+                            <div class="quick-intro">
+                                <div class="poster-title">
+                                    <h1>placeholder movie title</h1>
+                                    <h3>placeholder release date</h3>
+                                    <p>placeholder rating stars</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     <div class="movie-info-card">
                         <div class="poster">
