cb97ba5a7aefbae958b8aa8b447ca6fea051fbf7
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2021-01-10 20:54:49 -0800

    non-functional searchbar- but it looks nice!

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import search from '../images/searchButtonRed.png';
 
-const Search = () => (
-    /*return(
-    <input type="text" placeholder="Search..." class="search-bar"/>*/
-	<img src={ search } class="search-icon" />
-    /*)*/
-);
-
+const Search = () => {
+    return(
+    <div class="search-bar-container">
+        <input type="text" placeholder="Search..." class="search-bar"/>
+        <img src={ search } class="search-icon" />
+    </div>
+    );
+}
 export default Search;
