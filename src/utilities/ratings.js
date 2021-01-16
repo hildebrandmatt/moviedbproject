02468384968aa5f9a3e54b6ac3ee6472e20df5b1
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2021-01-15 17:00:47 -0800

    an attempt at star ratings that failed miserably
    here's some of the stuff anyways

---
+++ src/utilities/ratings.js
@@ -0,0 +1,23 @@
+import StarRatings from '../react-star-ratings';
+ 
+//crying noises
+class Foo extends Component {
+    changeRating( newRating, name ) {
+      this.setState({
+        rating: newRating
+      });
+    }
+ 
+    render() {
+      // rating = 2;
+      return (
+        <StarRatings
+          rating={this.movie.rating}
+          starRatedColor="red"
+          changeRating={this.changeRating}
+          numberOfStars={10}
+          name='rating'
+        />
+      );
+    }
+}
