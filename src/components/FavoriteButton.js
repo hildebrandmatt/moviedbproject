3d07cc633423e57e2383e69c7e80a5455f5c3a5b
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-01-16 16:40:40 -0800

    Favorite local storage almost working

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -5,34 +5,8 @@ import { useEffect, useState } from 'react';
 const FavoriteButton = props => {
 
     const [ favorites, setFavorites ] = useState([])
-    const getArray = JSON.parse(localStorage.getItem('favorites') || '0');
-
-    useEffect(() => {
-        if (getArray !== 0) {
-            setFavorites([...getArray])
-        }
-        console.log("hello")
-    }, [])
 
     const addFav = (props: any) => {
-        console.log(getArray)
-        //console.log(favorites)
-        let array = favorites;
-        //console.log(array);
-        let addArray = true;
-        array.map((item: any, key: number) => {
-            if ( item === props.movieID ) {
-                array.splice(key, 1);
-                addArray = false;
-                //console.log("hello")
-            }
-        })
-        if (addArray) {
-            array.push(props.movieID)
-        }
-        //console.log(array)
-        setFavorites([...array])
-        //console.log(favorites)
 
         localStorage.setItem("favorites", JSON.stringify(favorites));
 
@@ -42,13 +16,14 @@ const FavoriteButton = props => {
         } else {
             localStorage.removeItem('favMovie' + (props.movieID));
         }
+        setFavorites()
     }
 
     const movieID = props.movieID
     
     return (
         <div>
-            {favorites.includes(movieID) ? (
+            {localStorage.getItem('favMovie' + movieID) ? (
                 <img    class="favorite-button"
                         src={favoriteButton}
                         onClick={() => addFav({ movieID })} />
