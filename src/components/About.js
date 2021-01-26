736127ab11d972ab0ef4fbb69282fc5934cd23bc
nyanbeans <66818117+nyanbeans@users.noreply.github.com>
2021-01-25 19:07:28 -0800

    style finagling + about page stuff

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,27 @@ const About = () => {
             <div class="content-box">
                 <h1>About</h1>
                 <hr />
-                <p>Keep track of your favourite movies with Watcher. Our application allows
-                    users to browse through their favourite movies, check their ratings, and
-                    add them to their lists. Don't like a movie anymore? Removing it from your
-                    favourites is as easy as pressing the heart button on the posters.
-                </p>
-                <br></br>
-                <p>
-                This product uses the TMDb API but is not endorsed or certified by TMDb.
-                <br></br>
-                <br></br>
-                <img src={dbLogo} class="dblogo"/>
-                </p>
+                <div className="about-app-text">
+                    <div className="app-description">
+                        <h3>The Project</h3>
+                        <p>Watcher is a movie listing app that lets users browse movies based 
+                            on popularity, rating, and release date, and keep track of movies 
+                            they love.
+                        </p>
+                        <p>
+                            This product uses the TMDb API but is not endorsed or certified by TMDb.
+                        </p>
+                        <img src={dbLogo} class="dblogo"/>
+                    </div>
+                    <br></br>
+                    <div class='app-team'>
+                        <h3>The Team</h3>
+                        <p>
+
+                        </p>
+                    </div>
+                </div>
             </div>
-            
         </main>
     )
 }
