4c7232df4c3dc59b03718edf95b885a3c46c6e9a
Matt Hildebrand <hildebrand.matt@gmail.com>
2021-02-02 21:08:27 -0800

    Added basic search page pagination

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -10,23 +10,73 @@ import missingPoster from '../images/placeholderPoster.PNG'
 const SearchPage = () => {
 
     const [movieArray, setMovieArray] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
     
     function useQuery() {
         return new URLSearchParams(useLocation().search);
     }
 
     const query = useQuery().get('query');
-    var page = 1
 
     useEffect(() => {
+
+        if(query === null || query === ''){
+            return;
+        }
+
         const fetchMovies = async () => {
-            const res = await fetch(`${TMDB_SEARCH_URL}${query}&page=${page}&include_adult=false`)
+            const res = await fetch(`${TMDB_SEARCH_URL}${query}&page=${pageNumber}&include_adult=false`)
             let data = await res.json()
-            let processedData = await processMovieArray(data, 20)
+            let processedData = await processMovieArray(data, data.results.length)
             setMovieArray(processedData)
         }
         fetchMovies()
-    }, [query])
+    }, [query, pageNumber])
+
+
+    const PageSelector = () => {
+    
+        const pages = []
+    
+        var i = pageNumber - 4
+        if (i < 1) {
+            i = 1
+        }
+        var max = i + 9
+        var prev = pageNumber - 1
+        if (prev < 1) {
+            prev = 1
+        }
+        const next = pageNumber + 1
+    
+        for (i; i < max; i++ ) {
+            let y = i
+            if(i === pageNumber){
+                pages.push(<li id="current-page-selector" key={i}><div className="page-link" id="current-page" onClick={() => pageChange(y)}><p>{i}</p></div></li>)
+
+            } else if ( ( pageNumber < 4 && i > 5 ) ||
+                        ( pageNumber === 4 && i > 7 ) ){
+                            pages.push(<li className="hide-on-mobile" key={i}><div className="page-link" onClick={() => pageChange(y)}><p>{i}</p></div></li>)
+            } else {
+                pages.push(<li key={i}><div className="page-link" onClick={() => pageChange(y)}><p>{i}</p></div></li>)
+            }
+        }
+    
+        return (
+            <nav className="page-selector">
+                <div className="page-link" id="button-previous" onClick={() => pageChange(prev)}><p>Previous</p></div>
+                <ul>
+                {pages}
+                </ul>
+                <div className="page-link" id="button-next" onClick={() => pageChange(next)}><p>Next</p></div>
+            </nav>
+        )
+    };
+
+    const pageChange = (page) => {
+        setPageNumber(page)
+    }
+
 
     return (
         <main>
@@ -65,6 +115,7 @@ const SearchPage = () => {
                     })}
                 </div>
             </div>
+            <PageSelector />
         </main>
     );
 }
